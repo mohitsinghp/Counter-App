9e007b29fdb439fcd332aff50c3207ccf2567b76
Mohit Singh Parihar <mohitsinghparihar1511@gmail.com>
2021-10-26 19:15:41 +0530

    styles added

--- src/Counter.js
+++ src/Counter.js
@@ -17,18 +17,50 @@ const Counter = () => {
 
     const changeCount = (e) => {
         console.log(e.target.value)
-        setValue({count: e.target.value})
+        setValue({ count: e.target.value })
+    }
+
+    const mystyle = {
+        button: {
+            fontSize: "20px",
+            color: "brown",
+            background: "none",
+            height: "50px",
+            width: '50px',
+            padding: "0",
+            border: "none",
+        },
+        container: {
+            border: "1px solid brown",
+            borderRadius: "5px",
+            display: "flex",
+            alignItems: "center"
+        },
+        input: {
+            fontSize: "20px",
+            border: "none",
+            color: "brown",
+            backgroundColor: "#e6bb9e",
+            borderColor: "brown",
+            width: '50px',
+            height: "51px",
+            textAlign: "center",
+            padding: "0"
+        }
     }
 
     return (
-        <div>
-          <button onClick={minusCount}>-</button>
-          <input type="text"
-          name="count" 
-          value={value.count}
-          placeholder={value.count}
-          onChange={changeCount} />  
-          <button onClick={addCount}>+</button>
+        <div style={mystyle.container}>
+            <button style={mystyle.button}
+                onClick={minusCount}>-</button>
+            <input type="text"
+                style={mystyle.input}
+                name="count"
+                value={value.count}
+                placeholder={value.count}
+                onChange={changeCount} />
+            <button style={mystyle.button}
+                onClick={addCount}>+</button>
         </div>
     )
 }
--- src/CounterValue.js
+++ src/CounterValue.js
@@ -3,8 +3,20 @@ import { CountContext } from './Context'
 
 const CounterValue = () => {
     const [value, setValue] = useContext(CountContext);
+    const mystyle = {
+        button: {
+            fontSize: "20px",
+            color: "brown",
+            background: "none",
+            height: "50px",
+            width: '50px',
+            padding: "0",
+            border: "none",
+        }
+    }
+
     return (
-        <div>
+        <div style={mystyle.container}>
             <span>Counter Value</span><br />
             <span>{value.count}</span>
         </div>
